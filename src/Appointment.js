2908b8a125fa4fd80ebf2fda2f7a8c73811c9766
codingwidad <brafik.widad@gmail.com>
2024-12-19 15:42:27 +0000

    Url generated by render

--- src/Appointment.js
+++ src/Appointment.js
@@ -12,7 +12,7 @@ const Appointment = () => {
 
   const onSubmit = async (data) => {
     try {
-      const response = await fetch("http://localhost:5000/api/appointments", {
+      const response = await fetch("https://barbershop-backend-6rw5.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
